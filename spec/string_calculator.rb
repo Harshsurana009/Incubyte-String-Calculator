03e7737a275b205bd5365a67a13b0e34478a07bc
Harshsurana009 <harshsurana009@gmail.com>
2024-05-27 17:21:59 +0530

    Test: Added test for single number input

--- spec/string_calculator.rb
+++ spec/string_calculator.rb
@@ -6,4 +6,8 @@ RSpec.describe StringCalculator do
   it 'should returns 0 for an empty string' do
     expect(StringCalculator.add('')).to eq(0)
   end
+
+  it 'Should return same number when single number given' do
+    expect(StringCalculator.add("1")).to eq(1)
+  end
 end
