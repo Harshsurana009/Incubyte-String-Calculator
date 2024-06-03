486a21f5220b3320c97212bf73128d5829ff0732
Harshsurana009 <harshsurana009@gmail.com>
2024-06-03 17:34:01 +0530

    Added support for any user input validations

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,20 +1,48 @@
 # frozen_string_literal: true
 
 # lib/string_calculator.rb
+
+# users can add their custom validations to the calculator
+# Ex: calculator = StringCalculator.new
+# calculator.add_validation do |num|
+#   raise "Negative numbers not allowed: #{num}" if num < 0
+# end
+
 class StringCalculator
-  def self.add(input)
+  def initialize
+    @validations = []
+  end
+
+  def add(input)
     return 0 if input.empty?
 
-    numbers = if input.start_with?('//')
-                delimiter, input = input[2..-1].split("\n", 2)
-                input.split(delimiter).map(&:to_i)
-              else
-                input.split(/,|\n/).map(&:to_i)
-              end
-
-    negatives = numbers.select(&:negative?)
-    raise "Negative numbers not allowed: #{negatives.join(',')}" unless negatives.empty?
+    numbers = extract_numbers(input)
+    check_validity_of_input(numbers)
 
     numbers.sum
+  end
+
+  def add_validation(&block)
+    @validations << block
+  end
+
+  private
+
+  def check_validity_of_input(numbers)
+    numbers.each do |num|
+      @validations.each do |validation|
+        validation.call(num.to_i)
+      end
+    end
+  end
+
+  def extract_numbers(input)
+    delimiter, numbers = extract_delimiter_and_number(input)
+
+    delimiter.nil? ? input.split(/,|\n/).map(&:to_i) : numbers.split(delimiter).map(&:to_i)
+  end
+
+  def extract_delimiter_and_number(input)
+    input.start_with?('//') ? input[2..-1].split("\n", 2) : [nil, input]
   end
 end
--- spec/string_calculator.rb
+++ spec/string_calculator.rb
@@ -1,37 +1,73 @@
 # Spec for Incubyte string calculator
 require 'rspec'
-require File.expand_path('../lib/string_calculator', __dir__)
+require File.expand_path('../lib/string_ccalculator', __dir__)
 
 RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
   it 'should returns 0 for an empty string' do
-    expect(StringCalculator.add('')).to eq(0)
+    expect(calculator.add('')).to eq(0)
   end
 
   it 'Should return same number when single number given' do
-    expect(StringCalculator.add('1')).to eq(1)
+    expect(calculator.add('1')).to eq(1)
   end
 
   it 'Should returns the sum of two numbers' do
-    expect(StringCalculator.add('1,2')).to eq(3)
+    expect(calculator.add('1,2')).to eq(3)
   end
 
   it 'Should returns the sum of multiple numbers' do
-    expect(StringCalculator.add('1,2,5')).to eq(8)
+    expect(calculator.add('1,2,5')).to eq(8)
   end
 
   it 'Should handles new lines between numbers' do
-    expect(StringCalculator.add("1\n2,3")).to eq(6)
+    expect(calculator.add("1\n2,3")).to eq(6)
   end
 
   it 'Should supports different delimiters' do
-    expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    expect(calculator.add("//;\n1;2")).to eq(3)
   end
 
-  it 'Should raises an exception for negative numbers' do
-    expect { StringCalculator.add('1,-2,3') }.to raise_error('Negative numbers not allowed: -2')
+  context 'when input contains negative numbers' do
+    before do
+      calculator.add_validation do |num|
+        raise "Negative numbers not allowed: #{num}" if num < 0
+      end
+    end
+
+    it 'Should raises an error for negative numbers' do
+      expect { calculator.add('1,-2,3') }.to raise_error('Negative numbers not allowed: -2')
+    end
   end
 
-  it 'Should raises an exception with all negative numbers' do
-    expect { StringCalculator.add('1,-2,-3') }.to raise_error('Negative numbers not allowed: -2,-3')
+  context 'when input contains even numbers and even numbers are not allowed' do
+    before do
+      calculator.add_validation do |num|
+        raise "Even numbers not allowed: #{num}" if num.even?
+      end
+    end
+
+    it 'Should raises an error for even numbers' do
+      expect { calculator.add('1,2,3') }.to raise_error('Even numbers not allowed: 2')
+    end
+  end
+
+  context 'Should when input contains prime numbers and prime numbers are not allowed' do
+    before do
+      calculator.add_validation do |num|
+        def prime?(num)
+          return false if num <= 1
+
+          (2..Math.sqrt(num)).none? { |i| num % i == 0 }
+        end
+
+        raise "Prime numbers not allowed: #{num}" if prime?(num)
+      end
+    end
+
+    it 'Should raises an error for prime numbers' do
+      expect { calculator.add('2,3,5') }.to raise_error('Prime numbers not allowed: 2')
+    end
   end
 end
